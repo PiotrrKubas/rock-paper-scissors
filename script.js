0452e4c90d918fce3e01d9ff04601fdba1a07334
PiotrrKubas <piotrkubas39@gmail.com>
2026-03-19 20:10:46 +0100

    getComputerChoice() completed

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Hello world!");
+// INITIALIZE consts rock, paper, scissors
+// USE Math.random to return 0 or 1 or 2
+// IF random = 0 return rock
+// IF random = 1 return paper
+// IF random = 2 return scissors
+
+function getComputerChoice(){
+    const rock = "rock";
+    const paper = "paper";
+    const scissors = "scissors";
+    const max = 3;
+    const random = Math.floor(Math.random() * max);
+
+    if (random === 0)
+    {
+        return rock;
+    }
+    else if (random === 1)
+    {
+        return paper;
+    }
+    else 
+    {
+        return scissors;
+    }
+}
+
+console.log(getComputerChoice())
